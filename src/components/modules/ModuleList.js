4e9d410b36618c112ee2f251a9e14eb026e30577
Aditya Banerjee <aditya_banerjee@icloud.com>
2021-12-27 20:24:10 +0800

    Bug fixes

--- src/components/modules/ModuleList.js
+++ src/components/modules/ModuleList.js
@@ -2,14 +2,14 @@ import Module from './Module'
 
 function ModuleList(props) {
     return <ul>
-        {props.modules.map(module => (
-            <Module
+        {props.modules.map(module => ( module.description.length > 200 ?
+            (<Module
                 key = {module.id}
                 code = {module.moduleCode}
                 title = {module.title}
                 description = {module.description}
                 mcs = {module.moduleCredit}
-            />)
+            />) : null)
         )}
     </ul>
 }
